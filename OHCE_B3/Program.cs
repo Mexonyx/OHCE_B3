00bbd9a8decfcae293dbb32a343e29ddce7823f3
MAXIME LEDRU <maxime.ledru4@orange.fr>
2023-01-29 10:50:34 +0100

    Etape 1 terminee

---
+++ OHCE_B3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+static string getMirrorString(string str)
+{
+    StringBuilder stringBuilder = new StringBuilder();
+
+    var reversed = new string(str.Reverse().ToArray());
+
+    stringBuilder.Append(reversed);
+
+    return stringBuilder.ToString();
+}
+
+Console.WriteLine("Bonjour !");
+
+string inputUSer = Console.ReadLine();
+
+string reversedInputUser = getMirrorString(inputUSer);
+
+Console.WriteLine(reversedInputUser);
+
+if(inputUSer == reversedInputUser)
+{
+    Console.WriteLine("Bien dit !");
+}
+
+Console.WriteLine("Au revoir");
+
+
+
+
